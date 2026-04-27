e8202084c976484a667900544938fe48f01d8157
Cerinnelovesonnyangels <olmedo.cs@obmontessori.edu.ph>
2026-04-27 21:55:45 +0800

    Update alt.py

--- alt.py
+++ alt.py
@@ -3,15 +3,15 @@ from pyscript import document # type: ignore
 Olmedo = [
     {
         "img": "https://i.pinimg.com/736x/4d/21/e1/4d21e1c2c27672c316f284d44a03ea83.jpg",
-        "caption": "Description"
+        "caption": "Cerinne's description"
     },
     {
         "img": "https://i.pinimg.com/1200x/6b/6a/ab/6b6aab076c556b70d434d7b848806ef4.jpg",
-        "caption": "Description"
+        "caption": "Lauren's description"
     },
     {
         "img": "https://i.pinimg.com/736x/30/8f/66/308f66f49f56ba3957be226d6c25117c.jpg",
-        "caption": "Description"
+        "caption": "Jai's description"
     }
 ]
 
@@ -19,15 +19,15 @@ Olmedo = [
 Baring = [
     {
         "img": "https://i.pinimg.com/736x/5e/17/d0/5e17d09d26f477c08755907890032884.jpg",
-        "caption": "Description"
+        "caption": "Aisha's description"
     },
     {
         "img": "https://i.pinimg.com/736x/4d/21/e1/4d21e1c2c27672c316f284d44a03ea83.jpg",
-        "caption": "Description"
+        "caption": "Shalanie's description"
     },
     {
         "img": "https://i.pinimg.com/1200x/6b/6a/ab/6b6aab076c556b70d434d7b848806ef4.jpg",
-        "caption": "Description"
+        "caption": "Amanda's description"
     }
 ]
 
