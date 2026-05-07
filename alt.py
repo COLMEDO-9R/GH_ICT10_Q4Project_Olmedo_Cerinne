6413ed81d8b59a3a7f6ac4eed454784e28188890
Cerinnelovesonnyangels <olmedo.cs@obmontessori.edu.ph>
2026-05-07 13:10:09 +0800

    Update alt.py

--- alt.py
+++ alt.py
@@ -26,7 +26,7 @@ Baring = [
         "caption": "my favorite memory is 2025-2026 Campout <br> - Shalanie Garabiles"
     },
     {
-        "img": "Food Fair_OBMC.jpg",
+        "img": "food fair.jpg",
         "caption": "my favorite memory is the Food Fair <br> - Amanda Yao"
     }
 ]
